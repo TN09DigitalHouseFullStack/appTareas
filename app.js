2385706bb7ac06c81fa14129953df8796724886d
daduque <daanduque@utp.edu.co>
2022-03-09 20:06:10 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const tareasJSON = require('./leerTareas');
+
+//Obtengo la palabra que necesito en la ejecucion de node app.js listar
+let accion = process.argv[??];
+// console.log(accion);
+
+//Realizo la evaluacion de casos
+switch (accion) {
+    case 'listar':
+        //Ciclar e imprimir las tareas con el formato #. titulo - estado
+
+        break;
+    case 'listarTerminadas':
+        //Igual a listar solo que ahora solo listo las tareas terminadas
+
+        break;
+    case undefined:
+        //Inidicarle al usuario adicionalmente las acciones disponibles
+        console.log('Atencion debe ingresar una accion');
+        break;
+    default:
+        //Inidicarle al usuario adicionalmente las acciones disponibles
+        console.log('No entiendo');
+        break;
+}
+
+// listar
+
+// listarProgreso
+
+// listarTerminadas
+
+// listarPenndiente
